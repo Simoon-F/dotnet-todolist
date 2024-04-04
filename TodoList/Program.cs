bcdba4105f0d62acc56a04167e75176a40431d7d
Simon <simoonf@vip.qq.com>
2024-04-04 16:38:05 +0800

    feat: 注册数据库上下文
    1. 添加 using 指令。
    2. 将数据库上下文添加到 DI 容器。
    3. 指定数据库上下文将使用内存中数据库。

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoList.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
